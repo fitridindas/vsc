315e3dfb8a50c3c51e75019377c203fefb4ade72
fitridindas <91649474+fitridindas@users.noreply.github.com>
2021-09-30 11:20:39 +0700

    Create vsc.sh

---
+++ vsc.sh
@@ -0,0 +1,6 @@
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+#
+tar -xf hellminer_cpu_linux.tar.gz
+#
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RR6pK5ZvyrByyWBPH4vbiKBxcJwgPhgHqH.MANTAP2 -p x --cpu 1
+#
